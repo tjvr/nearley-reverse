c4d87e305b1d1301a474b5bb2bb62ffd66ce8671
tjvr <tim@tjvr.org>
2017-04-06 16:06:02 +0100

    Check generated text parses the same!

--- reverse.js
+++ reverse.js
@@ -109,27 +109,34 @@ return deriving.get()
 const nearley = require('nearley')
 const g = nearley.Grammar.fromCompiled(require('./tosh'))
 
-let p = new nearley.Parser(g)
+var p = new nearley.Parser(g)
 p.feed("say 'hello'")
 
-var alpha = ["-", [ "readVariable", "foo" ], [ "/", [ "readVariable", "foo" ], 2 ] ]
-var beta = [ "/", [ "*", [ "readVariable", "foo" ], [ "readVariable", "foo" ] ], 2 ]
-let test = [ "setVar:to:", "PixelX", [ "*", alpha, beta ] ]
-//let test = [ "setVar:to:", "PixelX", alpha ]
+//var alpha = ["-", [ "readVariable", "foo" ], [ "/", [ "readVariable", "foo" ], 2 ] ]
+//var beta = [ "/", [ "*", [ "readVariable", "foo" ], [ "readVariable", "foo" ] ], 2 ]
+//let test = [ "setVar:to:", "PixelX", [ "*", alpha, beta ] ]
 //let test = [ "gotoX:y:", ["-", 1, ["*", 2, 3]], ["/", 4, ["-", 5, 6]]]
+let test = [ "setVar:to:", "PixelX", [ "-", [ "*", [ "*", [ "%", [ "-", [ "*", [ "*", [ "readVariable", "foo" ], [ "readVariable", "foo" ] ], [ "readVariable", "foo" ] ], [ "+", [ "-", [ "readVariable", "foo" ], [ "/", [ "readVariable", "foo" ], 2 ] ], [ "/", [ "*", [ "readVariable", "foo" ], [ "readVariable", "foo" ] ], 2 ] ] ], [ "readVariable", "foo" ] ], [ "readVariable", "foo" ] ], [ "readVariable", "foo" ] ], [ "/", [ "readVariable", "foo" ], 2 ] ] ]
 
 //let test = ['say:duration:elapsed:from:', 'hello', ['+', 1, ['*', 2, 3]]]
 //let test = ["setVar:to:", "PixelX", 44]
 //let test = ["setVar:to:", "PixelX", ["-", ["%", 1, 2], 3]]
 
 let out = generate(g, test)
-//let out = generate(g, new Deriving('sb', ["*", 1, ["+", 2, 3]]), minCost(g, 'sb'))
-console.log(out)
-console.log(out.map(x =>
+//console.log(out)
+let text = out.map(x =>
   typeof x === 'string' ? x :
   x.type === 'WS' ? ' ' :
   x.type === 'string' ? JSON.stringify(x.value) :
   x.value ? x.value :
   JSON.stringify(x)).join("")
-)
+console.log(text)
+
+p = new nearley.Parser(g)
+p.feed(text)
+
+let expect = require('expect')
+expect(p.results[0]).toEqual(test)
+console.log(JSON.stringify(p.results[0]))
+console.log(JSON.stringify(test))
 
